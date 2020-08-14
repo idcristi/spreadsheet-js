fb7deb29b3a777487dc88eec2ce161119ddab495
idcristi <cristi.duluta@gmail.com>
2020-08-14 18:40:09 +0300

    Implement event listeners

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,10 +3,23 @@ import {SpreadsheetComponent} from '@core/SpreadsheetComponent';
 export class Formula extends SpreadsheetComponent {
   static className = 'spreadsheet__formula';
 
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click'],
+    });
+  }
+
   toHTML() {
     return `
       <div class="info">fx</div>
       <div class="input" contenteditable="" spellcheck="false"></div>
     `;
   }
+
+  onInput() {
+    console.log('Formula: onInput', event.target.textContent.trim());
+  }
+
+  onClick() {}
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,32 @@
+import {capitalize} from './utils';
+
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error(`No $root provided for DomListener`);
     }
     this.$root = $root;
+    this.listeners = listeners;
   }
+
+  initDOMListeners() {
+    // console.log(this.listeners, this.$root);
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        const name = this.name || '';
+        throw new Error(
+            `Method ${method} is not implemented in ${name} Component`,
+        );
+      }
+      console.log(method);
+      this.$root.on(listener, this[method].bind(this));
+    });
+  }
+
+  removeDOMListeners() {}
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
 }
